44e0b7624afe32b279e87a1a9d381c1177741521
tiken14 <tiken14@live.com>
2024-05-16 13:41:13 +0500

    Построение необходимых функций

---
+++ src/widget.py
@@ -0,0 +1,11 @@
+from masks import mask_card_numbers
+
+
+def get_masked_data(unmasked_data: str) -> str:
+    """ Функция принимает данные карты/счёта и возвращает их маски для сокрытия данных в виде строки """
+    pass
+
+
+def get_date(date_string: str) -> str:
+    """ Функция принимает дату в виде строки и возращает дату в необходимом формате """
+    pass
