2e55ee553cc0163f8d5a9c04ac2a80ebebb1d738
tiken14 <tiken14@live.com>
2024-05-17 09:18:18 +0500

    Исправление линтерами

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from src.widget import get_masked_data, get_date
 
 
-print(get_masked_data('Счет 64686473678894779589'))
+print(get_masked_data("Счет 64686473678894779589"))
 
 
-print(get_date('2018-07-11T02:26:18.671407'))
+print(get_date("2018-07-11T02:26:18.671407"))
--- src/widget.py
+++ src/widget.py
@@ -1,15 +1,14 @@
 from .masks import mask_card_numbers
 from .custom_errors import CardInfoError, DateFormatError
-import datetime
 
 
 def get_masked_data(unmasked_data: str) -> str:
-    """ Функция принимает данные карты/счёта и возвращает их маски для сокрытия данных в виде строки """
+    """Функция принимает данные карты/счёта и возвращает их маски для сокрытия данных в виде строки"""
 
     # Делим имя и номер карты/счета на две переменных. Для маскировки номера используем \
     # уже созданную функцию mask_card_numbers из другого модуля
-    operation_name = ' '.join([x for x in unmasked_data.split() if x.isalpha()])
-    nums = ''.join([x for x in unmasked_data if x.isdigit()])
+    operation_name = " ".join([x for x in unmasked_data.split() if x.isalpha()])
+    nums = "".join([x for x in unmasked_data if x.isdigit()])
 
     if not operation_name or not nums:
         raise CardInfoError()
@@ -18,13 +17,13 @@ def get_masked_data(unmasked_data: str) -> str:
 
 
 def get_date(date_string: str) -> str:
-    """ Функция принимает дату в виде строки и возращает дату в необходимом формате """
+    """Функция принимает дату в виде строки и возращает дату в необходимом формате"""
 
     # Отсекаем часы, оставляя только дату
-    date = date_string.split('T', 1)[0]
+    date = date_string.split("T", 1)[0]
 
     # Делим строку с датой на список, в котором будут: год, месяц, день
-    formated_date_list = date.split('-')
+    formated_date_list = date.split("-")
 
     # Проверка на кол-во элементов списка. Вызов исключения если кол-во не равно 3 (ожидается [год, месяц, день])
     if len(formated_date_list) != 3:
