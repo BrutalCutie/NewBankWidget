4888ef6894e73cad57285af06a6d5f2b1b4b4d9e
tiken14 <tiken14@live.com>
2024-05-22 10:21:06 +0500

    Готовая функция get_state

--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,11 @@
 def get_state(operations: list, state: str = 'EXECUTED') -> list:
     """Функция возвращает список словарей в которых ключ state == параметру функции state(по умолчанию EXECUTED"""
-    pass
+    return [operation for operation in operations if operation['state'] == state]
 
 
 def get_by_date_operations(operations: list, new_first: bool = True) -> list:
     """Функция возвращает упорядоченный список словарей по дате"""
     pass
+
+
+
