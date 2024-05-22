d37b646a3d16d30b32d0a4f9aa5f5293f33cc251
tiken14 <tiken14@live.com>
2024-05-22 16:07:06 +0500

    Исправление бага в модуле processing.py, функции get_by_date_operations

--- src/processing.py
+++ src/processing.py
@@ -1,6 +1,3 @@
-from src.widget import get_date
-
-
 def get_state(operations: list, state: str = "EXECUTED") -> list:
     """Функция возвращает список словарей в которых ключ state == параметру функции state(по умолчанию EXECUTED"""
     return [operation for operation in operations if operation["state"] == state]
@@ -8,4 +5,4 @@ def get_state(operations: list, state: str = "EXECUTED") -> list:
 
 def get_by_date_operations(operations: list, new_first: bool = True) -> list:
     """Функция возвращает упорядоченный список словарей по дате. По умолчанию(по убыванию)"""
-    return sorted(operations, key=lambda op: get_date(op["date"]), reverse=new_first)
+    return sorted(operations, key=lambda op: op["date"], reverse=new_first)
