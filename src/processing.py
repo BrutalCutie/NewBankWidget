235d8b79b1906b1db4159703841e9684a86510da
tiken14 <tiken14@live.com>
2024-05-22 10:14:47 +0500

    Создание README файла. Создание модуля processing.py Подготовка функций

---
+++ src/processing.py
@@ -0,0 +1,8 @@
+def get_state(operations: list, state: str = 'EXECUTED') -> list:
+    """Функция возвращает список словарей в которых ключ state == параметру функции state(по умолчанию EXECUTED"""
+    pass
+
+
+def get_by_date_operations(operations: list, new_first: bool = True) -> list:
+    """Функция возвращает упорядоченный список словарей по дате"""
+    pass
