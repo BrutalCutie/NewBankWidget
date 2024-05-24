9ca5ad6a33c8392a0abe5342c853f19cfa3c62c6
tiken14 <tiken14@live.com>
2024-05-24 10:10:45 +0500

    Исправления линтерами. Изменения конфигурации .toml (exclude). Переименование папки test в tests

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -3,7 +3,9 @@ import pytest
 from src.masks import mask_card_numbers
 
 
-@pytest.mark.parametrize("nums, expected", (
+@pytest.mark.parametrize(
+    "nums, expected",
+    (
         ("1596837868705199", "1596 83** **** 5199"),
         ("64686473678894779589", "**9589"),
         ("7158300734726758", "7158 30** **** 6758"),
@@ -11,6 +13,7 @@ from src.masks import mask_card_numbers
         ("6831982476737658", "6831 98** **** 7658"),
         ("8990922113665229", "8990 92** **** 5229"),
         ("899092211366522912312311523523523", "**3523"),
-))
+    ),
+)
 def test_mask_card_numbers(nums, expected):
     assert mask_card_numbers(nums) == expected
--- tests/test_widget.py
+++ tests/test_widget.py
@@ -1,28 +1,36 @@
 import pytest
+
+from src.custom_errors import CardInfoError, DateFormatError
 from src.widget import get_date, get_masked_data
-from src.custom_errors import DateFormatError, CardInfoError
 
 
 @pytest.mark.parametrize(
-    "str_date, expected", [
+    "str_date, expected",
+    [
         ("2019-07-03T18:35:29.512364", "03.07.2019"),
         ("2018-10-14T08:21:33.419441", "14.10.2018"),
         ("2018-09-12T21:27:25.241689", "12.09.2018"),
-        ("2018-06-30T02:08:58.425572", "30.06.2018")])
+        ("2018-06-30T02:08:58.425572", "30.06.2018"),
+    ],
+)
 def test_get_date(str_date, expected):
     assert get_date(str_date) == expected
 
 
 def test_get_date_wrong_format():
     with pytest.raises(DateFormatError):
-        get_date('2018-30T02:08:58')
+        get_date("2018-30T02:08:58")
 
 
-@pytest.mark.parametrize("unmasked_data, expected", [
-    ("Счет 73654108430135874305", "Счет **4305"),
-    ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
-    ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
-    ("Visa Platinum 8990 9221 1366 5229", "Visa Platinum 8990 92** **** 5229")])
+@pytest.mark.parametrize(
+    "unmasked_data, expected",
+    [
+        ("Счет 73654108430135874305", "Счет **4305"),
+        ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
+        ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
+        ("Visa Platinum 8990 9221 1366 5229", "Visa Platinum 8990 92** **** 5229"),
+    ],
+)
 def test_get_masked_data(unmasked_data, expected):
     assert get_masked_data(unmasked_data) == expected
 
