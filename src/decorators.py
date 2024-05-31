cffa72aa3ac6bfd180c29b28cf0b485378588f96
tiken14 <tiken14@live.com>
2024-05-31 10:57:41 +0500

    Работа с логгированием

--- src/decorators.py
+++ src/decorators.py
@@ -1,17 +1,40 @@
-def log(filepath: str = None, exc_info: bool = False):
-    """
-    Функция-декоратор.
-    Не изменяет поведение оборачиваемой функции.
-    Производит логгирование выполнения работы функции.
-    Если аргумент filepath остаётся пустым(по умолчанию None) - вывод прооиходит на консоль.
-    В ином случае создаёт файл, с логами об успешном выполнении или об ошибке которая произошла при выполнении.
+import logging
+from typing import Callable, Any, Union
 
-    :param filepath: по умолчанию None. Название файла для логгирования.
-    :param exc_info: по умолчанию False. Вывод подробной информации об ошибке.
-    :return:
+
+def log(filepath: str | None = None, exc_info: bool = False) -> Union[Callable, None]:
     """
-    def wrapper(func):
-        def inner(*args, **kwargs):
-            pass
+        Функция-декоратор.
+        Не изменяет поведение оборачиваемой функции.
+        Производит логгирование выполнения работы функции.
+        Если аргумент filepath остаётся пустым(по умолчанию None) - вывод прооиходит на консоль.
+        В ином случае создаёт файл, с логами об успешном выполнении или об ошибке которая произошла при выполнении.
+
+        :param filepath: по умолчанию None. Название файла для логгирования.
+        :param exc_info: по умолчанию False. Вывод подробной информации об ошибке.
+        :return: Фунцию или None в случае ошибки
+        """
+
+    def wrapper(func: Callable) -> Union[Callable, None]:
+        def inner(*args: tuple, **kwargs: dict) -> Any:
+            logging.basicConfig(filename=filepath, level=10)
+
+            try:
+                result = func(*args, **kwargs)
+                logging.debug(f"{func.__name__} Inputs: {args}, {kwargs} - ok")
+                return result
+            except Exception as err:
+                logging.error(f"{func.__name__} error: {err.__class__} Inputs: {args}, {kwargs}\n{str(err)}",
+                              exc_info=exc_info)
+                return None
+
         return inner
+
     return wrapper
+
+
+if __name__ == '__main__':
+
+    @log(exc_info=False)
+    def square(x: int, y=2):
+        return x * x * y
