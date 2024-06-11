d56e1867c01526e7201edc9174db1662276c2264
tiken14 <tiken14@live.com>
2024-06-11 14:18:16 +0500

    конец работы над функцией get_transactions_list_from_file

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,8 @@
+import json
+import sys
+import time
+
+
 def get_spaces_in_str(string: str, sep_every: int = 4, sep_symb: str = " ") -> str:
     """
     Функция принимает строку, вставляет sep_symb через каждые sep_every и возвращает новую строку
@@ -22,4 +27,45 @@ def get_spaces_in_str(string: str, sep_every: int = 4, sep_symb: str = " ") -> s
 
 
 def get_transactions_list_from_file(file_path: str) -> list:
-    pass
+    """
+    Функция принимает на вход путь до файла, в котором должен быть список
+    словарей.
+    Будет возвращен пустой список если:
+        Файл не найден | Файл содержит НЕ список | Файл пустой
+
+    пауза(time.sleep) вызывается для предотвращения "перепрыгивания" сообщений
+
+    :param file_path: путь до файла + название файла
+    :return: считанный с файла список словарей
+    """
+
+    # Оборачиваем код для отлавливания ошибки, когда файла не существует.
+    # В случае отсутствия - вернуть сообщение и пустой список
+
+    try:
+        with open(file_path, 'r', encoding='utf8') as file:
+
+            # Так как with open(... нельзя считать дважды, читаем и сохраняем результат чтения в переменную
+            static_file = file.read()
+
+            # Проверка что файл не пустой
+            if not len(static_file) > 0:
+                sys.stderr.write(f"NOTE: Expected list[dict] in file, got: nothing\n")
+                time.sleep(0.05)
+                return []
+
+            result = json.loads(static_file)
+
+            # Возвращаем пустой список если в файле НЕ список
+            if not isinstance(result, list):
+                file_type = type(result)
+                sys.stderr.write(f"NOTE: Expected contain: {type([])} in file, got: {file_type}\n")
+                time.sleep(0.05)
+                return []
+
+        return result
+
+    except FileNotFoundError:
+        sys.stderr.write(f"NOTE: File not founded, returned empty list\n")
+        time.sleep(0.05)
+        return []
