2a25ee78989a113c6ab0cbad85de4a9b2a0a6f94
tiken14 <tiken14@live.com>
2024-06-12 10:43:40 +0500

    тесты для external_api готовы

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,10 +1,29 @@
 from unittest.mock import patch, Mock
+
+import requests
+
 from src.external_api import get_amount_in_rubles, Converter
 
 
-def test_get_amount_in_rubles():
-    pass
+def test_get_amount_in_rubles(json_transactions_from_file):
+    mock_currency = Mock(return_value={'result': 89.321})
+    requests.get.json = mock_currency
+    for trans in json_transactions_from_file:
+        assert isinstance(get_amount_in_rubles(trans), float | None)
 
 
-def test_converter():
-    pass
+@patch('src.external_api.Converter.get_new_currency')
+def test_converter_get_currency(get_currency_mock):
+    get_currency_mock.return_value = 89.321
+
+    Converter.known_currencies['EUR'] = 100.0
+    assert Converter.get_currency('EUR') == 100.0
+    assert Converter.get_currency('USD') == 89.321
+
+
+@patch('requests.get')
+def test_converter_get_new_currency(get_currency_mock):
+    mock_work = get_currency_mock.return_value
+
+    mock_work.json.return_value = {'result': 89.321}
+    assert Converter.get_new_currency('USD') == 89.321
